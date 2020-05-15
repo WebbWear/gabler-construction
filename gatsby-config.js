1c51f3319beaf1c963eb77322dfe4a9f7d78ee46
Jeff Webb <webbwear@icloud.com>
2020-05-15 15:19:00 -0400

    install gatsby-image, transformer-sharp dependencies

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,5 +21,14 @@ module.exports = {
     },
     'gatsby-plugin-sass',
     'gatsby-plugin-offline',
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: path.join(__dirname, `src`, `images`),
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
   ],
 };
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -16,9 +16,9 @@ export default function Footer() {
         })}
       </ul>
       <ul className="copyright">
-        <li>&copy; Spectral</li>
+        <li>&copy; Gabler Brothers Inc</li>
         <li>
-          Design: <a href="http://html5up.net">HTML5 UP</a>
+          Design: <a href="http://webbwear.com">Webb Solutions</a>
         </li>
       </ul>
     </footer>
